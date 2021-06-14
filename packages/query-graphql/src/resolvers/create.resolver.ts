3aa4e5c3ebd8d98f93848ac279d86ff0b26f778d
Armando Soriano <43376080+asorian0@users.noreply.github.com>
2021-06-14 22:17:53 +0200

    feat: created resolver authorizer unique channel per user (#1)
    * feat: authorizer applied to pub sub
    
    * feat(query-graphql): authorizer for websocket in resolvers

--- packages/query-graphql/src/resolvers/create.resolver.ts
+++ packages/query-graphql/src/resolvers/create.resolver.ts
@@ -18,7 +18,7 @@ import {
   SubscriptionArgsType,
   SubscriptionFilterInputType,
 } from '../types';
-import { createSubscriptionFilter } from './helpers';
+import { createSubscriptionFilter, getUniqueNameForEvent } from './helpers';
 import { BaseServiceResolver, ResolverClass, ServiceResolver, SubscriptionResolverOpts } from './resolver.interface';
 import { OperationGroup } from '../auth';
 
@@ -147,7 +147,7 @@ export const Creatable = <DTO, C, QS extends QueryService<DTO, C, unknown>>(
       // Ignore `authorizeFilter` for now but give users the ability to throw an UnauthorizedException
       const created = await this.service.createOne(input.input.input);
       if (enableOneSubscriptions) {
-        await this.publishCreatedEvent(created);
+        await this.publishCreatedEvent(created, authorizeFilter);
       }
       return created;
     }
@@ -176,27 +176,38 @@ export const Creatable = <DTO, C, QS extends QueryService<DTO, C, unknown>>(
       // Ignore `authorizeFilter` for now but give users the ability to throw an UnauthorizedException
       const created = await this.service.createMany(input.input.input);
       if (enableManySubscriptions) {
-        await Promise.all(created.map((c) => this.publishCreatedEvent(c)));
+        await Promise.all(created.map((c) => this.publishCreatedEvent(c, authorizeFilter)));
       }
       return created;
     }
 
-    async publishCreatedEvent(dto: DTO): Promise<void> {
+    async publishCreatedEvent(dto: DTO, authorizeFilter?: Filter<DTO>): Promise<void> {
       if (this.pubSub) {
-        await this.pubSub.publish(createdEvent, { [createdEvent]: dto });
+        const eventName = authorizeFilter != null ? getUniqueNameForEvent(createdEvent, authorizeFilter) : createdEvent;
+        await this.pubSub.publish(eventName, { [createdEvent]: dto });
       }
     }
 
     @ResolverSubscription(() => DTOClass, { name: createdEvent, filter: subscriptionFilter }, commonResolverOpts, {
       enableSubscriptions: enableOneSubscriptions || enableManySubscriptions,
+      interceptors: [AuthorizerInterceptor(DTOClass)],
     })
-    // input required so graphql subscription filtering will work.
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    createdSubscription(@Args() input?: SA): AsyncIterator<CreatedEvent<DTO>> {
+
+    createdSubscription(
+      @Args() input?: SA,
+      @AuthorizerFilter({
+        operationGroup: OperationGroup.CREATE,
+        operationName: 'onCreateOne',
+        many: false,
+      })
+      authorizeFilter?: Filter<DTO>,
+    ): AsyncIterator<CreatedEvent<DTO>> | void {
       if (!this.pubSub || !(enableManySubscriptions || enableOneSubscriptions)) {
         throw new Error(`Unable to subscribe to ${createdEvent}`);
       }
-      return this.pubSub.asyncIterator<CreatedEvent<DTO>>(createdEvent);
+
+      const eventName = authorizeFilter != null ? getUniqueNameForEvent(createdEvent, authorizeFilter) : createdEvent;
+      return this.pubSub.asyncIterator<CreatedEvent<DTO>>(eventName);
     }
   }
   return CreateResolverBase;
